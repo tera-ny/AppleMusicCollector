f0111c32c9acb87bb9380cacc1ae3af7573f060f
g4zeru <haru25haru02@gmail.com>
2019-10-27 23:14:43 +0900

    InitializeCommit

---
+++ AppleMusicCollector/AppleMusicCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusicCollector
+//
+//  Created by iniad on 2019/10/27.
+//  Copyright © 2019 g4zeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
